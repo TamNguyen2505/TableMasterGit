172d97109ff878159f0bd69bd18f5ea8e9d17b1d
Minh Tam <tamnm1996@gmail.com>
2022-06-25 09:07:16 +0700

    configure ITune server

---
+++ TableGit/Server/SearchViewModel.swift
@@ -0,0 +1,59 @@
+//
+//  SearchViewModel.swift
+//  TableGit
+//
+//  Created by Nguyen Minh Tam on 25/06/2022.
+//
+
+import UIKit
+
+class SearchModel {
+    
+    //MARK: Properties
+    var url: URL?
+    private(set) var state: State = .notSearchedYet
+    
+    //MARK: Typelias
+    typealias SearchComplete = (Bool) -> Void
+    
+    
+    //MARK: API fuctions
+    func performSearch(for text: String, category: Category, completion: @escaping SearchComplete) {
+        
+        self.state = .loading
+        let url = ITuneService.iTinesUrl(searchText: text, category: Category(rawValue: category.rawValue) ?? .all)
+        self.url = url
+
+        ITuneService.fetchSearchResults(url: url, completion: { (searchResults, error) in
+            
+            if let error = error as NSError?, error.code == -999 {
+                
+                self.state = .notSearchedYet
+                DispatchQueue.main.async {
+    
+                    completion(false)
+                    
+                }
+                
+            }
+            
+            if searchResults.isEmpty {
+                
+                self.state = .noResults
+                
+            } else {
+                
+                self.state = .results(searchResults)
+                DispatchQueue.main.async {
+       
+                    completion(true)
+                }
+                
+            }
+            
+            
+        })
+        
+    }
+    
+}
