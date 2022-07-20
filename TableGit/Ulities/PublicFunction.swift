59ead48db436c6a9d848736891c9560781e54d4c
Nguyen Minh Tam <tamnm@minerva.vn>
2022-07-20 17:27:38 +0700

    clear trash

--- TableGit/Ulities/PublicFunction.swift
+++ TableGit/Ulities/PublicFunction.swift
@@ -7,26 +7,6 @@
 
 import UIKit
 
-func downloadImage(url: String, completion: @escaping (UIImage?) -> Void) {
-    
-    guard let url = URL(string: url) else {return}
-    
-    let session = URLSession.shared
-    let downloadTask = session.downloadTask(
-        with: url) { url, response, error in
-            
-            if error == nil, let url = url,
-               let data = try? Data(contentsOf: url),
-               let image = UIImage(data: data) {
-                completion(image)
-                
-            }
-            
-        }
-    
-    downloadTask.resume()
-}
-
 public func durationMeasurement(_ block: () -> ()) {
     let startTime = CFAbsoluteTimeGetCurrent()
     block()
