c42d9b51cf18cb06de9962ffa080f242747735f3
Nguyen Minh Tam <tamnm@minerva.vn>
2022-07-27 17:27:32 +0700

    clear trash

--- TableGit/ViewControllers/SettingsViewController.swift
+++ TableGit/ViewControllers/SettingsViewController.swift
@@ -45,6 +45,14 @@ class SettingsViewController: BaseViewController {
         return btn
     }()
     
+    struct SettingButton {
+        
+        var imageButton: UIImageView
+        var titleButton: UILabel
+        
+    }
+    
+    private var settingButtons = [SettingButton]()
     
     //MARK: View cycle
     override func setupUI() {
@@ -65,8 +73,85 @@ class SettingsViewController: BaseViewController {
             
         }
         
+        contentView.addSubview(avatarImageView)
+        avatarImageView.layer.cornerRadius = 120 / 2
+        avatarImageView.snp.makeConstraints{ make in
+            
+            make.top.equalToSuperview().offset(20)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(120)
+            make.bottom.equalToSuperview()
+            
+        }
+        
+        contentView.addSubview(uploadImageButton)
+        uploadImageButton.snp.makeConstraints{ make in
+            
+            make.width.height.equalTo(45)
+            make.top.equalTo(avatarImageView.snp.top)
+            make.trailing.equalToSuperview().inset(20)
+            
+        }
         
         
     }
+    
+    override func setupVM() {
+        super.setupVM()
+        
+        
+    }
+    
+    //MARK: Helpers
+    private func createSingleButton(nameOfImageButton: String, nameOfButton: String, optionalButton: UIControl? = nil) -> UIView {
+        
+        let outView = UIView()
+        outView.layer.cornerRadius = 5.0
+        outView.layer.borderWidth = 1.0
+        outView.layer.borderColor = UIColor.init(white: 0.5, alpha: 0.7).cgColor
+        outView.layer.shadowColor = UIColor.init(white: 0.6, alpha: 0.5).cgColor
+        outView.layer.shadowOffset = .init(width: 3, height: 3)
+        outView.layer.shadowRadius = 4.0
+        
+        let buttonImageView = UIImageView()
+        buttonImageView.image = UIImage(named: nameOfImageButton)
+        
+        let buttonNameTitle = UILabel()
+        buttonNameTitle.font = UIFont.systemFont(ofSize: 14)
+        buttonNameTitle.text = nameOfButton
+        
+        self.settingButtons.append(.init(imageButton: buttonImageView, titleButton: buttonNameTitle))
+        
+        let hStackHolder: UIStackView
+        
+        if let optionalButton = optionalButton {
+            
+            let hStack = UIStackView(arrangedSubviews: [buttonImageView, buttonNameTitle, optionalButton])
+            hStack.axis = .horizontal
+            hStackHolder = hStack
+            
+        } else {
+            
+            let arrowImageView = UIImageView()
+            arrowImageView.image = UIImage(named: "next")
+            
+            let hStack = UIStackView(arrangedSubviews: [buttonImageView, buttonNameTitle, arrowImageView])
+            hStack.axis = .horizontal
+            hStackHolder = hStack
+            
+        }
+        
+        outView.addSubview(hStackHolder)
+        hStackHolder.snp.makeConstraints { make in
+            
+            make.bottom.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(10)
+            
+        }
+        
+        return outView
+        
+    }
+    
     
 }
